23d75d75d6a105366ce0b045c840b2daff5ef941
Sange Lama <sangelama91@hotmail.com>
2022-02-22 12:14:54 +0530

    test

--- package.py
+++ package.py
@@ -1,10 +1,10 @@
 name = "mypackage"
-version = "1.0"
-build_command = False
+version = "2.0"
+build_command = "python {root}/install.py"
 
 
 def commands():
     global env
     env["MYVARIABLE"] = "YES"
     env["PYTHONPATH"].prepend("{root}")
-    env["PYTHONPATH"].prepend("{root}/python")
+    env["PYTHONPATH"].prepend("{root}/source")
